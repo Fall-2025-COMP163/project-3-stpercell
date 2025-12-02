9ec55c0685defe1146ce5a45599fd8bb6bc0be33
stpercell <stpercell@aggies.ncat.edu>
2025-12-02 02:08:04 -0500

    Update inventory_system.py

--- inventory_system.py
+++ inventory_system.py
@@ -2,9 +2,9 @@
 COMP 163 - Project 3: Quest Chronicles
 Inventory System Module - Starter Code
 
-Name: [Your Name Here]
+Name: [Shikel Percell]
 
-AI Usage: [Document any AI assistance used]
+AI Usage: [Mainly used for fromatting, debugging, and understanding]
 
 This module handles inventory management, item usage, and equipment.
 """
