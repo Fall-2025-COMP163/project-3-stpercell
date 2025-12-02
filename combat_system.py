4ea1dcd8a41de470ce8af56b992222817579e141
stpercell <stpercell@aggies.ncat.edu>
2025-12-02 02:05:54 -0500

    Update combat_system.py

--- combat_system.py
+++ combat_system.py
@@ -2,9 +2,9 @@
 COMP 163 - Project 3: Quest Chronicles
 Combat System Module - Starter Code
 
-Name: [Your Name Here]
+Name: [Shikel Percell]
 
-AI Usage: [Document any AI assistance used]
+AI Usage: [Mainly used help from the pevious project, ai for fromatting and understanding]
 
 Handles combat mechanics
 """
